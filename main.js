b42a8f2371a7e927540620663a7f2bfdb8cb64c2
Madhava Hansen <GSUSAWebsites@gmail.com>
2018-02-01 03:52:19 +0000

    testing clear form

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let form = document.getElementById("form");
-  form.on("submit", () => {
+  let form = $("form");
+  $form.on("submit", () => {
     let $input = $("submit-button");
     $input.text("");
     alert("working");
